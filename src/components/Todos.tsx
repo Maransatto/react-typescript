ab0a8ad3a68e7bf2d74078827c3adb4e1ba9d347
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2025-01-15 10:06:30 -0300

    feat: 590. Working with Props & Typescript

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,10 +1,18 @@
-function Todos() {
+import React, { PropsWithChildren } from "react";
+
+const Todos: React.FC<PropsWithChildren<{ items: string[] }>> = (props) => {
   return (
     <ul>
-      <li>Learn React</li>
-      <li>Learn Typescript</li>
+      {props.items.map((item) => (
+        <li key={item}>{item}</li>
+      ))}
     </ul>
   );
-}
+};
 
 export default Todos;
+
+// should I really use React.FC at this point?
+// export default function Todos(props: PropsWithChildren) {
+//   return <ul>{props.children}</ul>;
+// }
